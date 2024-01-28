ae542b622ffdb26628cc9ff12fe90983ac382cd6
Soporte Tepache <support@tepachesoft.com>
2024-01-28 14:05:59 -0700

    separate module in components

---
+++ src/app/components/section2.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+export default function AboutUs() {
+    return (
+        <section className="flex-col py-20 bg-gray-100">
+            <div className="text-black text-center">
+                <div className="flex flex-col items-center justify-center">
+                    <img className="w-16 h-16" src="https://aleypablo.nuestroinvitado.com/20deagosto/wp-content/uploads/elementor/thumbs/Icono-AyP-1-ox0d51d7sn3p8xmpe9lj31t3merqoalijt88tn98vw.png" alt="" />
+                </div>
+
+                <div className="flex flex-col items-center justify-center">
+                    <span className="text-[#C9511D] block text-4xl font-custom-serif mt-2 tracking-[1.5px]">Nuestra historia</span>
+                </div>
+
+                <div className="flex flex-col items-center justify-center mt-5 px-[46px]">
+                    <p className="block text-[16px] font-[300] font-custom-serif tracking-[1.5px]">Nuestra historia comienza un 18 de febrero del 2016 en una fiesta de una amiga en común, nos presentaron, empezamos a salir y finalmente nos convertimos en novios un 8 de mayo del mismo año. Hoy les anunciamos nuestro próximo matrimonio.</p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center pt-10">
+                    <span className="text-[#C9511D] block text-4xl font-custom-serif mt-2 tracking-[1.5px]">Adriana Hernandez</span>
+                </div>
+
+                <div className="flex flex-col items-center justify-center mt-5 px-[46px]">
+                    <p className="block text-[16px] font-[300] font-custom-serif tracking-[1.5px]">Nuestra historia comienza un 18 de febrero del 2016 en una fiesta de una amiga en común, nos presentaron, empezamos a salir y finalmente nos convertimos en novios un 8 de mayo del mismo año. Hoy les anunciamos nuestro próximo matrimonio.</p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center pt-4">
+                    <div className="p-4 flex justify-center items-center space-x-10">
+                        <a href="#" className="text-[#C9511D] text-[30px]">
+                            <i className="fab fa-facebook"></i>
+                        </a>
+                        <a href="#" className="text-[#C9511D] text-[30px]">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" className="text-[#C9511D] text-[30px]">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-center pt-10">
+                    <div className="w-80 h-80 rounded-[75px] overflow-hidden">
+                        <img className="w-full h-full object-cover" src="/juntos.png" alt="" />
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center justify-center pt-10">
+                    <span className="text-[#C9511D] block text-4xl font-custom-serif mt-2 tracking-[1.5px]">Jesús Vizcarra</span>
+                </div>
+
+                <div className="flex flex-col items-center justify-center mt-5 px-[46px]">
+                    <p className="block text-[16px] font-[300] font-custom-serif tracking-[1.5px]">Nuestra historia comienza un 18 de febrero del 2016 en una fiesta de una amiga en común, nos presentaron, empezamos a salir y finalmente nos convertimos en novios un 8 de mayo del mismo año. Hoy les anunciamos nuestro próximo matrimonio.</p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center pt-4">
+                    <div className="p-4 flex justify-center items-center space-x-10">
+                        <a href="#" className="text-[#C9511D] text-[30px]">
+                            <i className="fab fa-facebook"></i>
+                        </a>
+                        <a href="#" className="text-[#C9511D] text-[30px]">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" className="text-[#C9511D] text-[30px]">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+    );
+}
