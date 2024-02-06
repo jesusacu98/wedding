059eb90aada33c69ee2adbb85c4a50de36df4bc8
jesusacu98 <jesus_cuate98@hotmail.es>
2024-02-05 18:49:26 -0700

    add music in page

---
+++ src/app/components/musicPlayer.tsx
@@ -0,0 +1,38 @@
+// components/MusicPlayer.js
+import { useState, useRef } from 'react';
+
+const MusicPlayer = () => {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const [isPlaying, setIsPlaying] = useState(true);
+
+    const togglePlay = () => {
+        const audio = audioRef.current;
+
+        if (audio) {
+            if (isPlaying) {
+                audio.pause(); // Pausar la reproducción
+            } else {
+                audio.play(); // Reanudar la reproducción
+            }
+            setIsPlaying((prevState) => !prevState);
+        }
+    };
+
+    return (
+        <div className="fixed bottom-0 right-0 m-4 z-50">
+            <div className="rounded-full p-3" onClick={togglePlay}>
+                {isPlaying ? (
+                    <i aria-hidden="true" className='fa-solid fa-pause text-secondary text-4xl'></i>
+                ) : (
+                    <i aria-hidden="true" className='fa-solid fa-play text-secondary text-4xl'></i>
+                )}
+            </div>
+            <audio ref={audioRef} controls autoPlay className="my-custom-audio-style ml-2 hidden">
+                <source src="/cancion3.mp3" type="audio/mp3" />
+                Tu navegador no soporta la etiqueta de audio.
+            </audio>
+        </div>
+    );
+};
+
+export default MusicPlayer;
