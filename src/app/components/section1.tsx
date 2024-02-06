025253cb1485ad9ce57cf373fcca1287f3ea0b15
jesusacu98 <jesus_cuate98@hotmail.es>
2024-02-06 00:26:57 -0700

    add music

--- src/app/components/section1.tsx
+++ src/app/components/section1.tsx
@@ -4,11 +4,9 @@ import { useEffect, useRef, useState } from "react";
 export default function Portada() {
     const audioRef = useRef<HTMLAudioElement | null>(null);
     const [isPlaying, setIsPlaying] = useState(false);
-    const [hasScrolled, setHasScrolled] = useState(false);
 
     const togglePlay = () => {
         const audio = audioRef.current;
-
         if (audio) {
             if (isPlaying) {
                 audio.pause();
@@ -19,24 +17,22 @@ export default function Portada() {
         }
     };
 
-    const handleScroll = () => {
-        if (!hasScrolled) {
-            // Realiza acciones relacionadas con el primer scroll aquí
-            console.log("Usuario ha hecho el primer scroll");
-            togglePlay();
-            setHasScrolled(true); // Actualiza el estado para indicar que ya ha ocurrido el primer scroll
-        }
-    };
-
     useEffect(() => {
-        // Agrega el event listener al objeto window
-        window.addEventListener("scroll", handleScroll);
+        // Función para iniciar la reproducción cuando se hace clic en cualquier parte de la página
+        const handlePageClick = () => {
+            if (!isPlaying) {
+                togglePlay();
+            }
+        };
+
+        // Agrega el event listener al objeto document para capturar los clics en cualquier parte de la página
+        document.addEventListener("click", handlePageClick);
 
         // Limpia el event listener al desmontar el componente
         return () => {
-            window.removeEventListener("scroll", handleScroll);
+            document.removeEventListener("click", handlePageClick);
         };
-    }, [hasScrolled]);
+    }, [isPlaying]);
 
     return (
         <>
@@ -48,7 +44,7 @@ export default function Portada() {
                         <i aria-hidden="true" className='fa-solid fa-volume-xmark text-secondary text-4xl'></i>
                     )}
                 </div>
-                <audio ref={audioRef} controls autoPlay className="my-custom-audio-style ml-2 hidden">
+                <audio ref={audioRef} controls className="my-custom-audio-style ml-2 hidden">
                     <source src="/cancion.mp3" type="audio/mp3" />
                     Tu navegador no soporta la etiqueta de audio.
                 </audio>
