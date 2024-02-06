d93865961c6ac9bb7235e86252da8e3513cfd9b7
jesusacu98 <jesus_cuate98@hotmail.es>
2024-02-05 21:56:10 -0700

    add music

--- src/app/components/musicPlayer.tsx
+++ src/app/components/musicPlayer.tsx
@@ -10,9 +10,9 @@ const MusicPlayer = () => {
 
         if (audio) {
             if (isPlaying) {
-                audio.pause(); // Pausar la reproducción
-            } else {
                 audio.play(); // Reanudar la reproducción
+            } else {
+                audio.pause(); // Pausar la reproducción
             }
             setIsPlaying((prevState) => !prevState);
         }
@@ -25,13 +25,13 @@ const MusicPlayer = () => {
     return (
         <div className="fixed bottom-0 right-0 m-4 z-50">
             <div className="rounded-full p-3" onClick={togglePlay}>
-                {isPlaying ? (
+                {!isPlaying ? (
                     <i aria-hidden="true" className='fa-solid fa-volume-xmark text-secondary text-4xl'></i>
                 ) : (
                     <i aria-hidden="true" className='fa-solid fa-play text-secondary text-4xl'></i>
                 )}
             </div>
-            <audio ref={audioRef} controls autoPlay className="my-custom-audio-style ml-2 hidden">
+            <audio ref={audioRef} controls autoPlay className="my-custom-audio-style ml-2">
                 <source src="/cancion.mp3" type="audio/mp3" />
                 Tu navegador no soporta la etiqueta de audio.
             </audio>
